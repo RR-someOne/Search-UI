5dc3105683d01f97784e66c38a970d15db2d2c7f
Rohit Raikhy <raikhy101@gmail.com>
2025-10-29 19:56:21 -0400

    Add features for wireframe personal use tool

--- wireframealp/src/App.js
+++ wireframealp/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="spotify-app">
       <aside className="sidebar">
-        <div className="sidebar-logo">EHS</div>
+        <div className="sidebar-logo">Health and Wellness Personal Use</div>
         <nav className="sidebar-nav">
           <ul>
             <li className="active">Home</li>
@@ -19,7 +19,7 @@ function App() {
       <main className="main-content">
         <header className="main-header">
           <div className="header-left">
-             <div className="sidebar-logo">EHS Platform</div>
+             <div className="sidebar-logo">EHS Platform Personal Use</div>
           </div>
           <div className="header-right">
             <div className="profile-placeholder">Login</div>
@@ -28,11 +28,13 @@ function App() {
         <section className="section">
           <h2>Systems</h2>
           <div className="card-row">
-            <div className="card">Subsytem One</div>
-            <div className="card">Subsytem Two</div>
-            <div className="card">Subsytem Three</div>
-            <div className="card">Subsytem Four</div>
-            <div className="card">Subsytem Five</div>
+            <div className="card">Memory System Journaling</div>
+            <div className="card">Diet Tracking using LLM/Food based on illnesses</div>
+            <div className="card">Memoir LLM's with GenAI for pictures IOS/Google</div>
+            <div className="card">Advanced Task Manager based on memories/queries</div>
+            <div className="card">Search System for AI based Models/Medical Research/Finance</div>
+             <div className="card">EHS Records Systems</div>
+                          <div className="card">Travel</div>
           </div>
         </section>
       </main>
